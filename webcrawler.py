fe697e389ff000b5ccabd3c9c807e1e07c25a075
Nadvi Zaman <nadvi.zaman@student.manchester.ac.uk>
2022-12-03 15:34:06 +0000

    got start position of all hrefs

--- webcrawler.py
+++ webcrawler.py
@@ -1,7 +1,15 @@
 import urllib.request
+import re
 
 response = urllib.request.urlopen('https://news.ycombinator.com/') 
 
 html = str(response.read())
-print(type(html))
 
+test_sub = "href"
+ 
+# using re.finditer()
+# All occurrences of substring in string
+res = [i.start() for i in re.finditer(test_sub, html)]
+ 
+# printing result
+print("The start indices of the hrefs are : " + str(res))
