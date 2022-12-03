375c04b42972b01562c5e389355351762f71af2a
Nadvi Zaman <nadvi.zaman@student.manchester.ac.uk>
2022-12-03 15:57:55 +0000

    can get array of all https

--- webcrawler.py
+++ webcrawler.py
@@ -1,15 +1,31 @@
 import urllib.request
 import re
 
+def getJustHref(startPosit,theText):
+    endString = ""
+    loop = True
+    currentPos = startPosit
+    while (loop == True):
+        if(theText[currentPos] == '>'):
+            loop = False
+        else:
+            endString += theText[currentPos]
+            currentPos += 1
+    return(endString[7:len(endString)])
+
 response = urllib.request.urlopen('https://news.ycombinator.com/') 
 
 html = str(response.read())
 
-test_sub = "href"
+strToFind = 'a href="http'
  
 # using re.finditer()
 # All occurrences of substring in string
-res = [i.start() for i in re.finditer(test_sub, html)]
+res = [i.start() for i in re.finditer(strToFind, html)]
  
-# printing result
-print("The start indices of the hrefs are : " + str(res))
+
+hrefArray = []
+for x in range(0, len(res)):
+    hrefArray.append(getJustHref(res[x],html))
+
+print(hrefArray)
